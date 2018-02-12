a5f218e9836af71144e17655bdf3074971312ceb
Jordan Knight <jakkaj@gmail.com>
2018-02-12 12:26:32 +1100

    Added git puller script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# RightClickExplorerRunGit
-Git pull from explorer right click menu
+# Right Click Explorer Run Git Pull
+
+A PowerShell script to add your `git.exe` to a right click menu in Windows Explorer.
+
+## Usage
+
+Clone this repo. Run `gitpull.ps1` from an administrative command prompt. 
+
+Make sure you have `git.exe` in your environment path before running the script. 
+
+![Git pull here screenshot](https://user-images.githubusercontent.com/5225782/36081214-d14ae114-0fef-11e8-811b-2916a7837b91.png)
+
---
+++ gitpull.ps1
@@ -0,0 +1,44 @@
+$jpcommand = Get-Command git.exe | Select-Object -ExpandProperty Definition
+
+if($jpcommand -eq $null){
+    Write-Output "Could not find git.exe in environment path"
+    Break
+}
+
+Write-Output $jpcommand
+
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+
+function createRegedit([string]$registryPath, [string]$value)
+{
+    $name = "(Default)"
+
+    If(!(Test-Path $registryPath)){
+        New-Item -Path $registryPath -Force | Out-Null    
+    }
+    
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+        -PropertyType String -Force | Out-Null    
+}
+
+$openText = "Git Pull here"
+
+createRegedit "HKCR:\Directory\Background\shell\git" $openText
+createRegedit "HKCR:\Directory\shell\git" $openText
+#git.exe pull -v --progress "origin"
+$commandText = "`"" + $jpcommand + "`" --git-dir=`"%V/.git`" pull -v --progress"
+
+createRegedit "HKCR:\Directory\Background\shell\git\command" $commandText
+createRegedit "HKCR:\Directory\shell\git\command" $commandText
+
+$currentPath = (Get-Item -Path ".\" -Verbose).FullName
+
+#change to favicon-notebook.ico for alternative icon
+$jpicon = "$($currentPath)\gitfavicon.ico"
+
+#set icons
+New-ItemProperty -Path "HKCR:\Directory\Background\shell\git" -Name "Icon" -Value $jpicon `
+        -PropertyType String -Force | Out-Null    
+
+New-ItemProperty -Path "HKCR:\Directory\shell\git" -Name "Icon" -Value $jpicon `
+        -PropertyType String -Force | Out-Null  
